cd12fefd275b7574d5121356916c0adaaa423201
Yukio Ugajin <jinugasachio@gmail.com>
2021-09-18 15:49:15 +0900

    tfstateをs3で管理するように設定

--- autoscaling/provider.tf
+++ autoscaling/provider.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "architectures-terraform"
+    key    = "architectures-terraform/autoscaling"
+    region = "ap-northeast-1"
+  }
+}
+
 provider "aws" {
   region = "ap-northeast-1"
 }

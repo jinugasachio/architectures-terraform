febbd942a681f02fca5610fcebcb1fe483f2328a
Yukio Ugajin <jinugasachio@gmail.com>
2021-09-23 18:18:11 +0900

    autoscalingのdesired capacityを調整

--- autoscaling/autoscaling.tf
+++ autoscaling/autoscaling.tf
@@ -1,8 +1,8 @@
 resource "aws_autoscaling_group" "test" {
   name                = "test"
   min_size            = 1
-  max_size            = 2
-  desired_capacity    = 1
+  max_size            = 3
+  desired_capacity    = 3
   vpc_zone_identifier = module.vpc.private_subnets
   target_group_arns   = [aws_lb_target_group.test.arn]
 

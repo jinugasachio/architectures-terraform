b6e3ac45b2d56340aa67afd1ecab0d006634cb28
Yukio Ugajin <jinugasachio@gmail.com>
2021-09-30 22:03:53 +0900

    ロードバランサー用のセキュリティグループ追加

--- ecs_on_ec2/security_group.tf
+++ ecs_on_ec2/security_group.tf
@@ -33,3 +33,30 @@ resource "aws_security_group_rule" "ec2_sg_rule_for_outbound" {
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.ec2_sg.id
 }
+
+/**********************************
+* ロードバランサー用のセキュリティグループ
+**********************************/
+
+resource "aws_security_group" "lb_sg" {
+  name   = "ugajin-test"
+  vpc_id = module.vpc.vpc_id
+}
+
+resource "aws_security_group_rule" "lb_sg_rule_for_inbound" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.lb_sg.id
+}
+
+resource "aws_security_group_rule" "lb_sg_rule_for_outbound" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 65536
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.lb_sg.id
+}

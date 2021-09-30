6551a0347b855fed3414ba495f95138e616aeba4
Yukio Ugajin <jinugasachio@gmail.com>
2021-09-30 21:13:46 +0900

    stateをs3で管理する

--- ecs_on_ec2/terraform.tf
+++ ecs_on_ec2/terraform.tf
@@ -5,6 +5,12 @@ terraform {
       version = "3.60.0"
     }
   }
+
+  backend "s3" {
+    bucket = "architectures-terraform"
+    key    = "architectures-terraform/ecs_on_ec2"
+    region = "ap-northeast-1"
+  }
 }
 
 provider "aws" {

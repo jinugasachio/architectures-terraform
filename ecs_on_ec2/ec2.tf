6b76fc06e4304f645d8f9b0913555e4b01501dce
Yukio Ugajin <jinugasachio@gmail.com>
2021-09-30 23:03:50 +0900

    コンテナインスタンスの起動テンプレートを追加

---
+++ ecs_on_ec2/ec2.tf
@@ -0,0 +1,22 @@
+data "aws_ssm_parameter" "ecs_optimized_ami_image_id" {
+  name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id"
+}
+
+resource "aws_launch_template" "for_container_instance" {
+  name                   = "test"
+  image_id               = data.aws_ssm_parameter.ecs_optimized_ami_image_id.value
+  instance_type          = "t2.micro"
+  key_name               = "jinugasachioforaws.pem"
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+
+  iam_instance_profile {
+    arn = aws_iam_instance_profile.for_container_instance.arn
+  }
+
+  user_data = filebase64("./register_instance_with_cluster.sh")
+}
+
+resource "aws_iam_instance_profile" "for_container_instance" {
+  name = "test"
+  role = aws_iam_role.for_ecs_instance.name
+}
---
+++ ecs_on_ec2/register_instance_with_cluster.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+    
+echo ECS_CLUSTER=test >> /etc/ecs/ecs.config

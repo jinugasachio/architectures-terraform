e3714794eff1f77ee16ee10111a4ed26cf594dba
Yukio Ugajin <jinugasachio@gmail.com>
2021-09-30 23:09:40 +0900

    鍵名が間違っていたので修正

--- ecs_on_ec2/ec2.tf
+++ ecs_on_ec2/ec2.tf
@@ -6,7 +6,7 @@ resource "aws_launch_template" "for_container_instance" {
   name                   = "test"
   image_id               = data.aws_ssm_parameter.ecs_optimized_ami_image_id.value
   instance_type          = "t2.micro"
-  key_name               = "jinugasachioforaws.pem"
+  key_name               = "jinugasachioforaws"
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
   iam_instance_profile {

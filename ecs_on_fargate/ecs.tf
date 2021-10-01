3fc18f83f5f57395133d6b6e041b6fb790a1fcb0
Yukio Ugajin <jinugasachio@gmail.com>
2021-10-01 22:07:32 +0900

    fargateの場合ネットワークモードはaws_vpc

--- ecs_on_fargate/ecs.tf
+++ ecs_on_fargate/ecs.tf
@@ -12,7 +12,7 @@ resource "aws_ecs_task_definition" "example" {
   family                   = "example"
   cpu                      = "256"     # FARGATEの場合はrequired
   memory                   = "512"     # FARGATEの場合はrequired
-  network_mode             = "aws_vpc" # FARGATEの場合はrequired かつ aws_vpcを指定する
+  network_mode             = "aws_vpc" # FARGATEの場合はrequired かつ aws_vpcを指定する。https://docs.aws.amazon.com/ja_jp/AmazonECS/latest/developerguide/AWS_Fargate.html 
   requires_compatibilities = ["FARGATE"]
   container_definitions    = file("./container_definitions.json")
 }

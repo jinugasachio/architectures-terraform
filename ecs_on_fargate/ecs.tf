4e1da71aeaa5549ec3afeb033076326915befff3
Yukio Ugajin <jinugasachio@gmail.com>
2021-10-01 22:02:55 +0900

    タスク定義を追加

--- ecs_on_fargate/ecs.tf
+++ ecs_on_fargate/ecs.tf
@@ -7,3 +7,12 @@ resource "aws_ecs_cluster" "example" {
     value = "enabled"
   }
 }
+
+resource "aws_ecs_task_definition" "example" {
+  family                   = "example"
+  cpu                      = "256"     # FARGATEの場合はrequired
+  memory                   = "512"     # FARGATEの場合はrequired
+  network_mode             = "aws_vpc" # FARGATEの場合はrequired かつ aws_vpcを指定する
+  requires_compatibilities = ["FARGATE"]
+  container_definitions    = file("./container_definitions.json")
+}

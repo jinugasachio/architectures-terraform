ee7824bce9263f4c9d7466832289902956e08749
Yukio Ugajin <jinugasachio@gmail.com>
2021-10-01 22:17:40 +0900

    ecsのサービスを追加

--- ecs_on_fargate/ecs.tf
+++ ecs_on_fargate/ecs.tf
@@ -16,3 +16,19 @@ resource "aws_ecs_task_definition" "example" {
   requires_compatibilities = ["FARGATE"]
   container_definitions    = file("./container_definitions.json")
 }
+
+resource "aws_ecs_service" "example" {
+  name                              = "example"
+  cluster                           = aws_ecs_cluster.example.arn
+  task_definition                   = aws_ecs_task_definition.example.arn
+  desired_count                     = 2
+  launch_type                       = "FARGATE"
+  platform_version                  = "1.4.0" # デフォルトはLATESTだが、名前に反して最新ではない場合がある。ので明示するのがベストプラクティス。https://docs.aws.amazon.com/ja_jp/AmazonECS/latest/developerguide/platform_versions.html 
+  health_check_grace_period_seconds = 60
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.example.arn
+    container_name   = "example"
+    container_port   = 80
+  }
+}

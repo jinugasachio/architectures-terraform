2b811a05651a36063beca8917ee7d67a857798c3
Yukio Ugajin <jinugasachio@gmail.com>
2021-10-01 22:38:56 +0900

    Fix typo

--- ecs_on_fargate/ecs.tf
+++ ecs_on_fargate/ecs.tf
@@ -12,7 +12,7 @@ resource "aws_ecs_task_definition" "example" {
   family                   = "example"
   cpu                      = "256"     # FARGATEの場合はrequired
   memory                   = "512"     # FARGATEの場合はrequired
-  network_mode             = "aws_vpc" # FARGATEの場合はrequired かつ aws_vpcを指定する。https://docs.aws.amazon.com/ja_jp/AmazonECS/latest/developerguide/AWS_Fargate.html 
+  network_mode             = "awsvpc" # FARGATEの場合はrequired かつ aws_vpcを指定する。https://docs.aws.amazon.com/ja_jp/AmazonECS/latest/developerguide/AWS_Fargate.html 
   requires_compatibilities = ["FARGATE"]
   container_definitions    = file("./container_definitions.json")
 }
@@ -55,5 +55,5 @@ module "nginx_sg" {
   name        = "nginx-sg"
   vpc_id      = aws_vpc.example.id
   port        = 8080
-  cidr_blocks = [aws_vpc.example.cidr_blocks]
+  cidr_blocks = [aws_vpc.example.cidr_block]
 }

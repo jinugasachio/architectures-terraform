d4aa4701097419ff0f28cb2723ee84db6c0eb759
Yukio Ugajin <jinugasachio@gmail.com>
2021-10-02 18:14:22 +0900

    バッチ処理用コンテナ用のロググループ追加

--- ecs_on_fargate/cloudwatch.tf
+++ ecs_on_fargate/cloudwatch.tf
@@ -1,4 +1,11 @@
+# nginxコンテナのロギング
 resource "aws_cloudwatch_log_group" "for_ecs" {
   name              = "/ecs/example"
   retention_in_days = 180 # ログ保持日数
+}
+
+# バッチ処理用コンテナのロギング
+resource "aws_cloudwatch_log_group" "for_ecs_scheduled_tasks" {
+  name              = "/ecs-scheduled-tasks/example"
+  retention_in_days = 180
 }

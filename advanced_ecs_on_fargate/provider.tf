783fe511e5f78c2ea373ce39b198b7f43fe6ca34
Yukio Ugajin <jinugasachio@gmail.com>
2021-10-31 21:31:40 +0900

    Initializing the backend... Terraform has detected you're unconfiguring your previously set "s3" backend.
    Successfully unset the backend "s3". Terraform will now operate locally.
    
    Initializing provider plugins...
    - Reusing previous version of hashicorp/aws from the dependency lock file
    - Using previously-installed hashicorp/aws v3.63.0
    
    Terraform has been successfully initialized!
    
    You may now begin working with Terraform. Try running "terraform plan" to see
    any changes that are required for your infrastructure. All Terraform commands
    should now work.
    
    If you ever set or change modules or backend configuration for Terraform,
    rerun this command to reinitialize your working directory. If you forget, other
    commands will detect it and remind you to do so if necessary.

--- advanced_ecs_on_fargate/provider.tf
+++ advanced_ecs_on_fargate/provider.tf
@@ -5,12 +5,6 @@ terraform {
       version = "3.63.0"
     }
   }
-
-  backend "s3" {
-    bucket = "architectures-terraform"
-    key    = "architectures-terraform/advanced_ecs_on_fargate"
-    region = "ap-northeast-1"
-  }
 }
 
 provider "aws" {

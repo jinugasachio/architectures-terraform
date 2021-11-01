37be9d89a2dbfa2c5b6053dea3581bdef644eb96
jinugasachio <jinugasachio@gmail.com>
2021-11-01 22:34:12 +0900

    freeeのsandboxアカウント上のs3バケットでstateを管理

--- advanced_ecs_on_fargate/provider.tf
+++ advanced_ecs_on_fargate/provider.tf
@@ -5,8 +5,14 @@ terraform {
       version = "3.63.0"
     }
   }
+
+  backend "s3" {
+    bucket = "yukio-ugajin-test"
+    key    = "architectures-terraform/advanced_ecs_on_fargate" # freeeサンドボックス環境で管理なので注意
+    region = "us-west-2"
+  }
 }
 
 provider "aws" {
-  region = "ap-northeast-1"
+  region = "us-west-2"
 }

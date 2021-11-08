09098b499d02cf9acd18533ca86a38047c6a68ad
jinugasachio <jinugasachio@gmail.com>
2021-11-08 22:06:56 +0900

    tfstateのバックエンド先のバケットのキーを修正

--- advanced_ecs_on_fargate/environments/production/tfstate_backend.tf
+++ advanced_ecs_on_fargate/environments/production/tfstate_backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "yukio-ugajin-test"
-    key    = "architectures-terraform/advanced_ecs_on_fargate" # freeeサンドボックス環境で管理なので注意
+    key    = "architectures-terraform/advanced_ecs_on_fargate/production" # freeeサンドボックス環境で管理なので注意
     region = "us-west-2"
   }
 }
